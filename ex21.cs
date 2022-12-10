ec0857f549e8e37c5823394e62e8d63c7a396270
dbtb <mitbat@inbox.ru>
2022-12-10 21:34:47 +0300

    исправил 21

--- ex21.cs
+++ ex21.cs
@@ -6,9 +6,5 @@ Console.WriteLine("Введите координаты второй точки")
 double x2 = Convert.ToDouble(Console.ReadLine());
 double y2 = Convert.ToDouble(Console.ReadLine());
 double z2 = Convert.ToDouble(Console.ReadLine());
-double distance (double x1, double y1, double z1, double x2, double y2, double z2){
-double result = Math.Sqrt((Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2)));
-return result;
-}
-double r = distance (x1,x2,y1,y2,z1,z2);
+double r = Math.Sqrt((Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2)));
 Console.WriteLine($"Длина вектора равна {r}");
